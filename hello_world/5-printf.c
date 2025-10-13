b7382b1f27d40639f14c0c9c66765dbd9439a6c0
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-13 12:36:29 +0000

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * description: prints "With proper grammar, but the outcome is a piece of art,"
+ * description: prints "With proper grammar, but the outcome is a piece of art"
  *
  * Return: 0 on success
 */
 int main(void)
 {
-	printf("With proper grammar, but the outcome is a piece of art,\n");
+	printf("With proper grammar, but the outcome is a piece of art\n");
 	return (0);
 }
