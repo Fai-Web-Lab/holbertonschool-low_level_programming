477718ca78c875d96c05e596ea65b950b3e476e5
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-13 12:26:53 +0000

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: prints "With proper grammar, but the outcome is a piece of art,"
+ *
+ * Returns: 0 on success
+*/
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
