e6506ba9a3498efb1186417412523300e5221b9a
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-14 11:43:33 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,17 +8,16 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n <= 8; n++)
 	{
 		for (m = n + 1; m <= 9; m++)
 		{
 			putchar(n + '0');
 			putchar(m + '0');
 			if (n != 8 || m != 9)
-			{
 				putchar(',');
+			if (n != 8 || m != 9)
 				putchar(' ');
-			}
 		}
 	}
 	putchar('\n');
