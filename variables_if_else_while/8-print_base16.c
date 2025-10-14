12cdb919c8944ecbb04f4c32db27d6f6313e0a09
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-14 11:38:25 +0000

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 16; n++)
+		putchar(n < 10 ? n + '0' : n - 10 + 'a');
+	putchar('\n');
+	return (0);
+}
