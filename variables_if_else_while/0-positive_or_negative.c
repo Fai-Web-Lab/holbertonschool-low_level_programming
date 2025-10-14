f0142889d8020414658c12615ce6b52240bf176b
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-14 10:40:10 +0000

    Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
- * Description: Prints whether the number stored in n is positive, zero, or negative
+ *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	int n;
