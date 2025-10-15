ac7c496850e059e385f29a6b62c9b8af652b6f5f
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-15 14:27:41 +0000

    10x alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#include "main.h"
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ * main - Entry point
  *
- * Return: void
+ * Return: Always 0 (Success)
 */
-void print_alphabet(void)
+int main(void)
 {
-	char ch = 'a';
-
-	while (ch <= 'z')
-{
-	_putchar(ch);
-	ch++;
-}
-	_putchar('\n');
+	print_alphabet_x10();
+	return (0);
 }
