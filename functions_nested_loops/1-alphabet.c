62605517b30fca0c735e5708e6f4eb106293abf8
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-17 21:33:48 +0000

    adding new files

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,16 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ * print_alphabet - prints the alphabet in lowercase
  *
  * Return: void
 */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-	int i;
 	char ch;
 
-	for (i = 0; i < 10; i++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			_putchar(ch);
-		}
-		_putchar('\n');
+		_putchar(ch);
 	}
+	_putchar('\n');
 }
