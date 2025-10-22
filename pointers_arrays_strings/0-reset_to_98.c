a38f581831bbbda229848a5262b8df0fe29fc67a
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-22 14:06:34 +0000

    1st

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - updates the value pointed to by n to 98
- * @n: pointer to an int
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: Pointer to an integer whose value will be updated
+ *
+ * Return: Nothing (void)
 */
 void reset_to_98(int *n)
 {
