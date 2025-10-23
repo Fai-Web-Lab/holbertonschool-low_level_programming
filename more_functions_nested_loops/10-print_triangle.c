72616b5d4f27128fd7f487e24a6beef261ded34a
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-23 11:36:58 +0000

    last

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,33 @@
 #include "main.h"
 /**
- * print_triangle - prints a triangle using the character #
- * @size: the size of the triangle
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: the size (height) of the triangle
  *
- * Return: void
+ * Description: Prints a right-aligned triangle using the '#' character.
+ * If size is less than or equal to 0, the function prints only a new line.
 */
 void print_triangle(int size)
 {
-    int row, space, hash;
+	int row, space, hash;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (row = 1; row <= size; row++)
-    {
-        for (space = size - row; space > 0; space--)
-            _putchar(' ');
+	for (row = 1; row <= size; row++)
+	{
+		for (space = size - row; space > 0; space--)
+		{
+			_putchar(' ');
+		}
+		for (hash = 1; hash <= row; hash++)
+		{
 
-        for (hash = 0; hash < row; hash++)
-            _putchar('#');
+			_putchar('#');
+		}
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
