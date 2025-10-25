809e51573aa60700cd589b5e77d63c85df43a2d3
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-25 17:25:44 +0000

    1st

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - concatenates the string src to the end of dest
+ * @dest: destination string
+ * @src: source string to append
+ *
+ * Return: pointer to the resulting string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,6 @@
 
 #include <stdio.h>
 
-int _atoi(char *s);
+char *_strcat(char *dest, char *src);
 
 #endif
