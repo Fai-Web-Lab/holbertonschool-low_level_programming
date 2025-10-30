f7c916526aacd18cfe90472061f03f8dde05f425
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-10-30 15:39:28 +0000

    6th

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * find_sqrt - helper function to find the natural square root of a number
+ * @n: number to find the square root of
+ * @i: current number to test
+ *
+ * Return: the natural square root, or -1 if none found
+*/
+int find_sqrt(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (find_sqrt(n, i + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the natural square root of n, or -1 if n does not have one
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (find_sqrt(n, 0));
+}
