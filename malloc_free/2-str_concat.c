575fe0a51ee39f0a72b4efb70c9b8c45c4529c7f
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-11-04 11:52:22 +0000

    3rd

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,11 +11,10 @@
  *
  * Return: pointer to concatenated string, ot NULL on failure
 */
-char *str_concat(char *s1, *s2)
+char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	unsigned int i;
-	unsigned int j;
+	unsigned int i, j;
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
 
@@ -30,10 +29,12 @@ char *str_concat(char *s1, *s2)
 	concat = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (concat == NULL)
 		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
 	for (j = 0; j < len2; j++)
 		concat[i + j] = s2[j];
+
 	concat[i + j] = '\0';
 	return (concat);
 }
