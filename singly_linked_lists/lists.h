35ca61a0aff068c8132efbdfb2e9160a29d1fd21
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-11-24 16:32:28 +0000

    2nd task for the singly linked lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list node
  * @str: string
@@ -16,5 +18,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h)
 
 #endif
