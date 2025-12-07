f786ecded6b1afb0d8d16f024bfc6c7de6cdc384
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-07 15:45:23 +0000

    4th task in the FILE I/O project

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -36,26 +36,30 @@ int main(int argc, char *argv[])
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		close(fd_from);
 		error_exit(99, "Error: Can't write to", argv[2]);
-	}
+
 
 	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w != r)
-			error_exit(99, "error: can't write to %s\n", argv[2], -1);
+			error_exit(99, "error: can't write to", argv[2]);
 	}
 
 	if (r == -1)
 		error_exit(98, "Error: Can't read from file", argv[1]);
 
 	if (close(fd_from) == -1)
-		error_exit(100, "Error: Can't close fd", "fd_from");
+	{
+		dprintf(DTDERR_FILENO, "error: can't close fd %d\n", fd_from);
+		exit(100);
+	}
 
 	if (close(fd_to) == -1)
-		error_exit(100, "Error: Can't close fd", "fd_to");
+	{
+		dprintf(STDERR_FILENO, "error can't fd %d\n", fd_to);
+		exit(100);
+	}
 
 	return (0);
 }
