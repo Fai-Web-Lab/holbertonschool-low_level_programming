56cc8c7845326de1b1288e0248fe58809a564f93
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-07 16:07:09 +0000

    8th task in the doubly linked lists project

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given index
  * @h: Pointer to pointer to head of the list
@@ -16,27 +16,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     if (h == NULL)
         return (NULL);
 
-    /* Case 1: insert at beginning */
     if (idx == 0)
         return (add_dnodeint(h, n));
 
-    /* Traverse to node before insertion point */
+    tmp = *h;
     while (tmp != NULL && i < idx - 1)
     {
         tmp = tmp->next;
         i++;
     }
 
-    /* idx exceeds list length */
     if (tmp == NULL)
         return (NULL);
 
-    /* Case 2: insert at end */
     if (tmp->next == NULL)
         return (add_dnodeint_end(h, n));
 
     new_node = malloc(sizeof(dlistint_t));
-    if (new_node == N return (NULL);
+    if (new_node == NULL)
+	    return (NULL);
 
     new_node->n = n;
     new_node->prev = tmp;
