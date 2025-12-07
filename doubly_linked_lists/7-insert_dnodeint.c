cee8de86b166dc077f1c2393af672a0126fbc9ea
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-07 16:35:08 +0000

    8th task in the doubly linked lists project

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,38 +10,38 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *tmp = *h, *new_node;
-    unsigned int i = 0;
+	dlistint_t *tmp = *h, *new_node;
+	unsigned int i = 0;
 
-    if (h == NULL)
-        return (NULL);
+	if (h == NULL)
+		return (NULL);
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    tmp = *h;
-    while (tmp != NULL && i < idx - 1)
-    {
-        tmp = tmp->next;
-        i++;
-    }
+	tmp = *h;
+	while (tmp != NULL && i < idx - 1)
+	{
+		tmp = tmp->next;
+		i++;
+	}
 
-    if (tmp == NULL)
-        return (NULL);
+	if (tmp == NULL)
+		return (NULL);
 
-    if (tmp->next == NULL)
-        return (add_dnodeint_end(h, n));
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-	    return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = tmp;
-    new_node->next = tmp->next;
+	new_node->n = n;
+	new_node->prev = tmp;
+	new_node->next = tmp->next;
 
-    tmp->next->prev = new_node;
-    tmp->next = new_node;
+	tmp->next->prev = new_node;
+	tmp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
