96e17cca212e87cc72148164cc9f7ad840c21961
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-08 15:48:58 +0000

    3rd task in the hash tables project

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -3,7 +3,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif
