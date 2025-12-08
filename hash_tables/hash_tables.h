caf2c48053c83eb6cba774a370909b5c6abe8245
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-08 14:00:27 +0000

    2nd task in hash tables project

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,35 +1,9 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-#include <stddef.h>
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string (unique)
- * @value: The value corresponding to the key
- * @next: Pointer to the next node in the list (for collisions)
- *
- * Description: Node structure for a hash table using chaining.
-*/
-typedef struct hash_node_s
-{
-	char *key;
-	char *value;
-	struct hash_node_s *next;
-} hash_node_t;
+#include <stdlib.h>
+#include <stdio.h>
 
-/**
- * struct hash_table_s - Hash table data structure
- * @size: Size of the array
- * @array: Array of size @size, each cell points to a linked list head
- *
- * Description: Hash table structure using chaining collision handling.
-*/
-typedef struct hash_table_s
-{
-	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
-
-hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
