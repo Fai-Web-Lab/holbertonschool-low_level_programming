93e1fd6b6de3b7580ae86ed2ef796593cc935159
Fai-Web-Lab <fai.alsharekh@gmail.com>
2025-12-16 17:46:10 +0000

    add

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -62,7 +62,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
     idx = key_index((unsigned char *)key, ht->size);
     tmp = ht->array[idx];
-    
+
     while (tmp)
     {
         if (strcmp(tmp->key, key) == 0)
